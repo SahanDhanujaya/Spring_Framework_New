fa86f0f7d7a29fc8ddbfbed1b174696808627f8e
SahanDhanujaya <sahandhanujaya18@gmail.com>
2025-02-27 20:28:31 +0530

    back end update

--- 12_Spring_Data_JPA/src/main/java/lk/ijse/config/JPAConfig.java
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/config/JPAConfig.java
@@ -1,2 +1,56 @@
-package lk.ijse.config;public class JPAConfig {
+package lk.ijse.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+@EnableTransactionManagement
+@Configuration
+
+public class JPAConfig {
+    //connection -> mysql
+    //vendor -> hibernate
+    //transaction manager
+    //entity manager
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,JpaVendorAdapter jpaVendorAdapter){
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setJpaVendorAdapter(jpaVendorAdapter);
+        em.setPackagesToScan("lk.jse.entity");
+        return em;
+    }
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/spring?createDatabaseIfNotExist=true");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("95334238Sd$");
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(true);
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        return hibernateJpaVendorAdapter;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
 }
--- 12_Spring_Data_JPA/src/main/java/lk/ijse/repositories/CustomerRepository.java
+++ 12_Spring_Data_JPA/src/main/java/lk/ijse/repositories/CustomerRepository.java
@@ -1,2 +1,8 @@
-package lk.ijse.repositories;public interface CustomerRepository {
+package lk.ijse.repositories;
+
+import lk.ijse.dto.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer , Integer> {
+
 }
